72d2b389425b3952225696ff3f194a97445dbdfb
s-nakajima <nakajimashouhei@gmail.com>
2016-05-02 11:25:33 +0900

    phpdoc修正

--- Test/Case/Controller/TopicsControllerTest.php
+++ Test/Case/Controller/TopicsControllerTest.php
@@ -4,18 +4,17 @@
  *
  * @author Noriko Arai <arai@nii.ac.jp>
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
- * @author Noriko Arai <arai@nii.ac.jp>
- * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
  * @copyright Copyright 2014, NetCommons Project
-* @copyright Copyright 2014, NetCommons Project
  */
 
 App::uses('TopicsController', 'Topics.Controller');
 
 /**
  * TopicsController Test Case
+ *
+ * @package NetCommons\Topics\Test\Case\Controller
  */
 class TopicsControllerTest extends ControllerTestCase {
 
