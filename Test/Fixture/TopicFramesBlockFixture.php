9fad329031a171b2f6904f6c904281ebe995c623
s-nakajima <nakajimashouhei@gmail.com>
2016-09-07 13:09:28 +0900

    Fixtureの修正

--- Test/Fixture/TopicFramesBlockFixture.php
+++ Test/Fixture/TopicFramesBlockFixture.php
@@ -24,13 +24,15 @@ class TopicFramesBlockFixture extends CakeTestFixture {
  */
 	public $records = array(
 		array(
-			'id' => 1,
+			'id' => '1',
 			'frame_key' => 'Lorem ipsum dolor sit amet',
-			'block_id' => 1,
-			'created_user' => 1,
-			'created' => '2016-05-02 06:25:18',
-			'modified_user' => 1,
-			'modified' => '2016-05-02 06:25:18'
+			'room_id' => '1',
+			'plugin_key' => 'Lorem ipsum dolor sit amet',
+			'block_key' => 'Lorem ipsum dolor sit amet',
+			'created_user' => '1',
+			'created' => '2016-09-07 03:57:27',
+			'modified_user' => '1',
+			'modified' => '2016-09-07 03:57:27'
 		),
 	);
 
