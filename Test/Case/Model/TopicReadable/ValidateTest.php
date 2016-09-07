a4e24394cd5c4f7ae324e29e09a8662d7265c452
s-nakajima <nakajimashouhei@gmail.com>
2016-09-07 13:46:10 +0900

    TopicReadable::validate()のテスト

---
+++ Test/Case/Model/TopicReadable/ValidateTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * TopicReadable::validate()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsValidateTest', 'NetCommons.TestSuite');
+App::uses('TopicReadableFixture', 'Topics.Test/Fixture');
+
+/**
+ * TopicReadable::validate()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Topics\Test\Case\Model\TopicReadable
+ */
+class TopicReadableValidateTest extends NetCommonsValidateTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.categories.category',
+		'plugin.categories.category_order',
+		'plugin.topics.topic',
+		'plugin.topics.topic_frame_setting',
+		'plugin.topics.topic_frames_block',
+		'plugin.topics.topic_frames_plugin',
+		'plugin.topics.topic_frames_room',
+		'plugin.topics.topic_readable',
+		'plugin.topics.topic_user_status',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'topics';
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	protected $_modelName = 'TopicReadable';
+
+/**
+ * Method name
+ *
+ * @var string
+ */
+	protected $_methodName = 'validates';
+
+/**
+ * ValidationErrorのDataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - field フィールド名
+ *  - value セットする値
+ *  - message エラーメッセージ
+ *  - overwrite 上書きするデータ(省略可)
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderValidationError() {
+		$data['TopicReadable'] = (new TopicReadableFixture())->records[0];
+
+		return array(
+			// * topic_id
+			array('data' => $data, 'field' => 'topic_id', 'value' => 'a',
+				'message' => __d('net_commons', 'Invalid request.')),
+			// * user_id
+			array('data' => $data, 'field' => 'user_id', 'value' => 'a',
+				'message' => __d('net_commons', 'Invalid request.')),
+		);
+	}
+
+}
