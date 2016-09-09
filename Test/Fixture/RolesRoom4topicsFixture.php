b152f5a277b818324f542e8cbaae34943db9eb64
s-nakajima <nakajimashouhei@gmail.com>
2016-09-09 16:46:39 +0900

    Topic::__getRoomsConditions()のテスト

---
+++ Test/Fixture/RolesRoom4topicsFixture.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * RolesRoom4testFixture
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('RolesRoomFixture', 'Rooms.Test/Fixture');
+
+/**
+ * RolesRoom4testFixture
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Rooms\Test\Fixture
+ */
+class RolesRoom4topicsFixture extends RolesRoomFixture {
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	public $name = 'RolesRoom';
+
+/**
+ * Full Table Name
+ *
+ * @var string
+ */
+	public $table = 'roles_rooms';
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		//パブリックスペース
+		array('id' => '1', 'room_id' => '1', 'role_key' => 'room_administrator'),
+		array('id' => '2', 'room_id' => '1', 'role_key' => 'chief_editor'),
+		array('id' => '3', 'room_id' => '1', 'role_key' => 'editor'),
+		array('id' => '4', 'room_id' => '1', 'role_key' => 'general_user'),
+		array('id' => '5', 'room_id' => '1', 'role_key' => 'visitor'),
+		//パブリックスペース、パブリックルーム(room_id=4)
+		array('id' => '12', 'room_id' => '4', 'role_key' => 'room_administrator'),
+		array('id' => '13', 'room_id' => '4', 'role_key' => 'chief_editor'),
+		array('id' => '14', 'room_id' => '4', 'role_key' => 'editor'),
+		array('id' => '15', 'room_id' => '4', 'role_key' => 'general_user'),
+		array('id' => '16', 'room_id' => '4', 'role_key' => 'visitor'),
+		//プライベートスペース
+		array('id' => '6', 'room_id' => '2', 'role_key' => 'room_administrator'),
+		//プライベートルーム、管理者(room_id=5)
+		array('id' => '17', 'room_id' => '5', 'role_key' => 'room_administrator'),
+		//プライベートルーム、編集長(room_id=6)
+		array('id' => '18', 'room_id' => '6', 'role_key' => 'room_administrator'),
+		//プライベートルーム、編集者(room_id=7)
+		array('id' => '19', 'room_id' => '7', 'role_key' => 'room_administrator'),
+		//プライベートルーム、一般(room_id=8)
+		array('id' => '20', 'room_id' => '8', 'role_key' => 'room_administrator'),
+		//プライベートルーム、参観者(room_id=9)
+		array('id' => '21', 'room_id' => '9', 'role_key' => 'room_administrator'),
+		//コミュニティスペース
+		array('id' => '7', 'room_id' => '3', 'role_key' => 'room_administrator'),
+		array('id' => '8', 'room_id' => '3', 'role_key' => 'chief_editor'),
+		array('id' => '9', 'room_id' => '3', 'role_key' => 'editor'),
+		array('id' => '10', 'room_id' => '3', 'role_key' => 'general_user'),
+		array('id' => '11', 'room_id' => '3', 'role_key' => 'visitor'),
+		//コミュニティスペース、ルーム1(room_id=10)
+		array('id' => '22', 'room_id' => '10', 'role_key' => 'room_administrator'),
+		array('id' => '23', 'room_id' => '10', 'role_key' => 'chief_editor'),
+		array('id' => '24', 'room_id' => '10', 'role_key' => 'editor'),
+		array('id' => '25', 'room_id' => '10', 'role_key' => 'general_user'),
+		array('id' => '26', 'room_id' => '10', 'role_key' => 'visitor'),
+		//コミュニティスペース、ルーム1(room_id=11)
+		array('id' => '27', 'room_id' => '11', 'role_key' => 'room_administrator'),
+		array('id' => '28', 'room_id' => '11', 'role_key' => 'chief_editor'),
+		array('id' => '29', 'room_id' => '11', 'role_key' => 'editor'),
+		array('id' => '30', 'room_id' => '11', 'role_key' => 'general_user'),
+		array('id' => '31', 'room_id' => '11', 'role_key' => 'visitor'),
+	);
+
+}
---
+++ Test/Fixture/Room4topicsFixture.php
@@ -0,0 +1,219 @@
+<?php
+/**
+ * Room4testFixture
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('RoomFixture', 'Rooms.Test/Fixture');
+
+/**
+ * Room4testFixture
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Topics\Test\Fixture
+ */
+class Room4topicsFixture extends RoomFixture {
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	public $name = 'Room';
+
+/**
+ * Full Table Name
+ *
+ * @var string
+ */
+	public $table = 'rooms';
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		//パブリックスペース
+		array(
+			'id' => '1',
+			'space_id' => '2',
+			'page_id_top' => '4',
+			'parent_id' => null,
+			'lft' => '1',
+			'rght' => '2',
+			'active' => true,
+			'default_role_key' => 'visitor',
+			'need_approval' => '1',
+			'default_participation' => '1',
+			'page_layout_permitted' => '1',
+			'theme' => 'Default',
+		),
+		//パブリックスペース、パブリックルーム(room_id=4)
+		array(
+			'id' => '4',
+			'space_id' => '2',
+			'page_id_top' => '3',
+			'root_id' => '1',
+			'parent_id' => '1',
+			'lft' => '2',
+			'rght' => '3',
+			'active' => true,
+			'default_role_key' => 'visitor',
+			'need_approval' => true,
+			'default_participation' => true,
+			'page_layout_permitted' => true,
+			'theme' => null,
+		),
+
+		//プライベート
+		array(
+			'id' => '2',
+			'space_id' => '3',
+			'page_id_top' => null,
+			'parent_id' => null,
+			'lft' => '5',
+			'rght' => '16',
+			'active' => '1',
+			'default_role_key' => 'room_administrator',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => '0',
+			'theme' => 'Default',
+		),
+		//プライベートルーム、管理者(room_id=5)
+		array(
+			'id' => '5',
+			'space_id' => '3',
+			'page_id_top' => '0',
+			'root_id' => '2',
+			'parent_id' => '2',
+			'lft' => '6',
+			'rght' => '7',
+			'active' => true,
+			'default_role_key' => 'room_administrator',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => '0',
+			'theme' => null,
+		),
+		//プライベートルーム、編集長(room_id=6)
+		array(
+			'id' => '6',
+			'space_id' => '3',
+			'page_id_top' => '0',
+			'root_id' => '2',
+			'parent_id' => '2',
+			'lft' => '8',
+			'rght' => '9',
+			'active' => true,
+			'default_role_key' => 'room_administrator',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => '0',
+			'theme' => null,
+		),
+		//プライベートルーム、編集者(room_id=7)
+		array(
+			'id' => '7',
+			'space_id' => '3',
+			'page_id_top' => '0',
+			'root_id' => '2',
+			'parent_id' => '2',
+			'lft' => '10',
+			'rght' => '11',
+			'active' => true,
+			'default_role_key' => 'room_administrator',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => '0',
+			'theme' => null,
+		),
+		//プライベートルーム、一般(room_id=8)
+		array(
+			'id' => '8',
+			'space_id' => '3',
+			'page_id_top' => '0',
+			'root_id' => '2',
+			'parent_id' => '2',
+			'lft' => '12',
+			'rght' => '13',
+			'active' => true,
+			'default_role_key' => 'room_administrator',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => '0',
+			'theme' => null,
+		),
+		//プライベートルーム、参観者(room_id=9)
+		array(
+			'id' => '9',
+			'space_id' => '3',
+			'page_id_top' => '0',
+			'root_id' => '2',
+			'parent_id' => '2',
+			'lft' => '14',
+			'rght' => '15',
+			'active' => true,
+			'default_role_key' => 'room_administrator',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => '0',
+			'theme' => null,
+		),
+
+		//コミュニティスペース
+		array(
+			'id' => '3',
+			'space_id' => '4',
+			'page_id_top' => null,
+			'parent_id' => null,
+			'lft' => '17',
+			'rght' => '22',
+			'active' => '1',
+			'default_role_key' => 'general_user',
+			'need_approval' => '1',
+			'default_participation' => '1',
+			'page_layout_permitted' => '1',
+			'theme' => 'Default',
+		),
+		//コミュニティスペース、ルーム1(room_id=10)
+		array(
+			'id' => '10',
+			'space_id' => '4',
+			'page_id_top' => '0',
+			'root_id' => '3',
+			'parent_id' => '3',
+			'lft' => '18',
+			'rght' => '19',
+			'active' => '1',
+			'default_role_key' => 'general_user',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => null,
+			'theme' => null,
+		),
+		//コミュニティスペース、ルーム1(room_id=11)
+		array(
+			'id' => '11',
+			'space_id' => '4',
+			'page_id_top' => '0',
+			'root_id' => '3',
+			'parent_id' => '3',
+			'lft' => '20',
+			'rght' => '21',
+			'active' => '1',
+			'default_role_key' => 'general_user',
+			'need_approval' => '0',
+			'default_participation' => '0',
+			'page_layout_permitted' => null,
+			'theme' => null,
+		),
+	);
+
+}
