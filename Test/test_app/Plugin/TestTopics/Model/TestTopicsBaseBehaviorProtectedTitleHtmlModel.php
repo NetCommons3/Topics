349a42e11815d4bb076608e7bae21f102d112009
s-nakajima <nakajimashouhei@gmail.com>
2016-09-14 08:08:58 +0900

    imgタグのみの場合、タグがそのまま表示されていたが、「テキストなし」を表示するように修正 https://github.com/NetCommons3/NetCommons3/issues/669

---
+++ Test/test_app/Plugin/TestTopics/Model/TestTopicsBaseBehaviorProtectedTitleHtmlModel.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * TopicsBaseBehavior::_saveTopicReadable()テスト用Model
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AppModel', 'Model');
+
+/**
+ * TopicsBaseBehavior::_saveTopicReadable()テスト用Model
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Topics\Test\test_app\Plugin\TestTopics\Model
+ */
+class TestTopicsBaseBehaviorProtectedTitleHtmlModel extends AppModel {
+
+/**
+ * 使用ビヘイビア
+ *
+ * @var array
+ */
+	public $actsAs = array(
+		'Topics.TopicsBase' => array(
+			'fields' => array(
+				'title' => 'TestTopicsBaseBehaviorProtectedTitleHtmlModel.title'
+			),
+			'titleHtml' => true
+		),
+	);
+
+}
