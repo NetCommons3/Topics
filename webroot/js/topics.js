3e6141f65b0f8e016c6a48f420a61c6ab2017eaa
s-nakajima <nakajimashouhei@gmail.com>
2017-01-10 10:39:29 +0900

    gjslintエラー修正

--- webroot/js/topics.js
+++ webroot/js/topics.js
@@ -92,88 +92,88 @@ NetCommonsApp.controller('TopicsController',
     ['$scope', '$http', '$location', '$window', 'NC3_URL',
       function($scope, $http, $location, $window, NC3_URL) {
 
-      /**
-       * 新着データ
-       */
-      $scope.topics = [];
+        /**
+         * 新着データ
+         */
+        $scope.topics = [];
 
-      /**
-       * Initialize
-       *
-       * @return {void}
-       */
-      $scope.initialize = function(data) {
-        $scope.named = data['named'];
-        $scope.paging = data['paging'];
-        $scope.params = data['params'];
-      };
+        /**
+         * Initialize
+         *
+         * @return {void}
+         */
+        $scope.initialize = function(data) {
+          $scope.named = data['named'];
+          $scope.paging = data['paging'];
+          $scope.params = data['params'];
+        };
 
-      /**
-       * URLからデータ取得
-       *
-       * @return {void}
-       */
-      $scope.link = function($event) {
-        $location.hash(
-          'page:' + ($scope.paging['page']) + '&' + 'frame_id:' + $scope.params['frame_id']
-        );
-        $window.location.href = $event.target.href;
-        $event.preventDefault();
-      }
-
-      /**
-       * URLからデータ取得
-       *
-       * @return {void}
-       */
-      $scope.more = function(startPage, currentPage) {
-        var url = '/topics/topics/index';
-        if (! currentPage) {
-          currentPage = ($scope.paging['page'] + 1);
+        /**
+         * URLからデータ取得
+         *
+         * @return {void}
+         */
+        $scope.link = function($event) {
+          $location.hash(
+            'page:' + ($scope.paging['page']) + '&' + 'frame_id:' + $scope.params['frame_id']
+          );
+          $window.location.href = $event.target.href;
+          $event.preventDefault();
         }
-        if (! startPage) {
-          startPage = currentPage;
-        }
-        angular.forEach($scope.named, function(value, key) {
-          if (key === 'page' || key === 'startPage') {
-          } else {
-            url = url + '/' + key + ':' + value;
+
+        /**
+         * URLからデータ取得
+         *
+         * @return {void}
+         */
+        $scope.more = function(startPage, currentPage) {
+          var url = '/topics/topics/index';
+          if (! currentPage) {
+            currentPage = ($scope.paging['page'] + 1);
           }
-        });
-        url = url + '/page:' + currentPage;
-        url = url + '/startPage:' + startPage;
-
-        $http.get(NC3_URL + url + '.json', {params: $scope.params})
-            .then(function(response) {
-              var data = response.data;
-              $scope.paging = data['paging'];
-              angular.forEach(data['topics'], function(value) {
-                $scope.topics.push(value);
-              });
-            },
-            function(response) {
-            });
-      };
-
-      /**
-       * hashChange
-       *
-       * @return {void}
-       */
-      $scope.hashChange = function() {
-        $($window).bind('hashchange', function() {
-          var page = $window.location.href.match('page:([0-9]+)');
-          var frameId = $window.location.href.match('frame_id:([0-9]+)');
-          if (page && frameId && frameId[1] == $scope.params['frame_id']) {
-            $scope.more(2, page[1]);
-            try {
-              var element = $('#topic-frame-' + $scope.params['frame_id']);
-              var pos = element.offset().top;
-              $window.scrollTo(0, pos - 200);
-            } catch (err) {
+          if (! startPage) {
+            startPage = currentPage;
+          }
+          angular.forEach($scope.named, function(value, key) {
+            if (key === 'page' || key === 'startPage') {
+            } else {
+              url = url + '/' + key + ':' + value;
             }
-          }
-        }).trigger('hashchange');
-      };
+          });
+          url = url + '/page:' + currentPage;
+          url = url + '/startPage:' + startPage;
 
-    }]);
+          $http.get(NC3_URL + url + '.json', {params: $scope.params})
+              .then(function(response) {
+                var data = response.data;
+                $scope.paging = data['paging'];
+                angular.forEach(data['topics'], function(value) {
+                  $scope.topics.push(value);
+                });
+              },
+              function(response) {
+              });
+        };
+
+        /**
+         * hashChange
+         *
+         * @return {void}
+         */
+        $scope.hashChange = function() {
+          $($window).bind('hashchange', function() {
+            var page = $window.location.href.match('page:([0-9]+)');
+            var frameId = $window.location.href.match('frame_id:([0-9]+)');
+            if (page && frameId && frameId[1] == $scope.params['frame_id']) {
+              $scope.more(2, page[1]);
+              try {
+                var element = $('#topic-frame-' + $scope.params['frame_id']);
+                var pos = element.offset().top;
+                $window.scrollTo(0, pos - 200);
+              } catch (err) {
+              }
+            }
+          }).trigger('hashchange');
+        };
+
+      }]);
